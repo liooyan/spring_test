65837b787b655d7ee2d1708c21fcb00bb1a312b4
liy <1064012668@qq.com>
2021-01-25 16:36:13 +0800

    项目创建

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author top.lioyan
+ * @version 1.0
+ * @date 2021/1/25 4:33 下午
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(1111);
+    }
+}
+
