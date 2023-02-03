e224132bf143a779331ae96f24693119dc6e3e9b
李延 <liyan@wondersoft.cn>
2023-02-03 15:45:34 +0800

    完成EnvironmentPostProcessorApplicationListener 的初步解析

--- spring-boot/src/main/java/cn/lioyan/boot/env/EnvironmentPostProcessorApplicationListener.java
+++ spring-boot/src/main/java/cn/lioyan/boot/env/EnvironmentPostProcessorApplicationListener.java
@@ -1,10 +1,15 @@
 package cn.lioyan.boot.env;
 
+import org.springframework.boot.env.EnvironmentPostProcessor;
+
+
 /**
  * {@link org.springframework.boot.env.EnvironmentPostProcessorApplicationListener}
  *  配置 环境变量。 监听：ApplicationEnvironmentPreparedEvent 、ApplicationPreparedEvent、ApplicationFailedEvent
+ * 通过spring.factories 加载{@link EnvironmentPostProcessor} 对象，对Environment 做后置出来， 优先度：MIN+10
  *
- *
+ * 在spring-boot 中 默认有6个
+ //TODO  需要解析
  * @author com.lioyan
  * @date 2023/2/3  15:38
  */
