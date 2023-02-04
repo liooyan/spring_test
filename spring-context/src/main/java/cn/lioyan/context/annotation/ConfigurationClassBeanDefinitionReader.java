0ce646ef03fffd2916b07c05f35e0429129346d4
liyan <lioyan0009338@163.com>
2023-02-04 20:38:06 +0800

    完善 ConfigurationClassBeanDefinitionReader

---
+++ spring-context/src/main/java/cn/lioyan/context/annotation/ConfigurationClassBeanDefinitionReader.java
@@ -0,0 +1,25 @@
+package cn.lioyan.context.annotation;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionEvaluator;
+import org.springframework.context.annotation.Conditional;
+
+/**
+ * {@link  org.springframework.context.annotation.ConfigurationClassBeanDefinitionReader}<br>
+ * loadBeanDefinitionsForConfigurationClass 方法：判断通过注解 Configuration 注入的bean条件<br>
+ * registerBeanDefinitionForImportedConfigurationClass 判断通过 注解 @Import 注入的bean条件<br>
+ * loadBeanDefinitionsForBeanMethod 判断通过 注解 @Bean 注入的bean条件<br>
+ * <br>
+ * <br>
+ * <br>
+ * 上面的所有方法都是通过 {@link ConditionEvaluator} 进行判断<br>
+ * 所有内容都是通过{@link Conditional} 注解里的value值进行判断 它的值为{@link Condition} 子类<br>
+ * 在spring-boot 中有注入<br>
+ *
+ * <br>
+ * <br>
+ * <br>
+ *
+ */
+public class ConfigurationClassBeanDefinitionReader {
+}
