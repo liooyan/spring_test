fcfc6b36d41cf8cef8c4d5aea4e87f2b897bb622
liyan <lioyan0009338@163.com>
2023-02-05 22:28:00 +0800

    继续梳理aop

---
+++ spring-aop/src/main/java/cn/lioyan/aop/aspectj/AspectJExpressionPointcut.java
@@ -0,0 +1,8 @@
+package cn.lioyan.aop.aspectj;
+
+/**
+ * {@link org.springframework.aop.aspectj.AspectJExpressionPointcut}
+ * Pointcut 代理条件的表达式
+ */
+public class AspectJExpressionPointcut {
+}
---
+++ spring-aop/src/main/java/cn/lioyan/aop/aspectj/AspectJMethodBeforeAdvice.java
@@ -0,0 +1,10 @@
+package cn.lioyan.aop.aspectj;
+
+/**
+ * {@link org.springframework.aop.aspectj.AspectJMethodBeforeAdvice}
+ *
+ *  封装的 @Before 这些方法，
+ *
+ */
+public class AspectJMethodBeforeAdvice {
+}
