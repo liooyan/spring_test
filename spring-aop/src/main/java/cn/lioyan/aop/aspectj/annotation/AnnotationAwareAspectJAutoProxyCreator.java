1da792e78a8031845f30ec7606321af3c83a29cf
李延 <liyan@wondersoft.cn>
2023-02-06 18:07:27 +0800

    完成 aop

--- spring-aop/src/main/java/cn/lioyan/aop/aspectj/annotation/AnnotationAwareAspectJAutoProxyCreator.java
+++ spring-aop/src/main/java/cn/lioyan/aop/aspectj/annotation/AnnotationAwareAspectJAutoProxyCreator.java
@@ -1,8 +1,13 @@
 package cn.lioyan.aop.aspectj.annotation;
 
 /**
- * {@link org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator}
- * aop 的BeanPostProcessor 。关于aop 的切面与注解都是通过它加载的
+ * {@link org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator} <br>
+ * aop 的BeanPostProcessor 。关于aop 的切面与注解都是通过它加载的.<br>
+ * 主要有： getEarlyBeanReference 和 postProcessAfterInitialization 两个阶段。<br>
+ * 如果有早期依赖，就通过getEarlyBeanReference 否则 postProcessAfterInitialization<br>
+ * 都是{@link org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator#wrapIfNecessary(Object, String, Object)} 实现。<br>
+ * 切面 就是扫描所有的beanDefinition。通过{@link AspectJAdvisorFactory} 创建<br>
+ * 然后通过{@link cn.lioyan.aop.framework.ProxyFactory} 创建切面<br>
  */
 public class AnnotationAwareAspectJAutoProxyCreator {
 }
