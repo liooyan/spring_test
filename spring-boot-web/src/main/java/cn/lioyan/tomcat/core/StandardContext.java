c3678e958259f9044f667ca7eed105701019570d
李延 <liyan@wondersoft.cn>
2023-02-09 09:45:56 +0800

    tomcat 完成 有需要在不错

---
+++ spring-boot-web/src/main/java/cn/lioyan/tomcat/core/StandardContext.java
@@ -0,0 +1,11 @@
+package cn.lioyan.tomcat.core;
+
+/**
+ * {@link org.apache.catalina.core.StandardContext}
+ *
+ * @author com.lioyan
+ * @date 2023/2/9  9:29
+ */
+public class StandardContext
+{
+}
--- spring-boot-web/src/main/java/cn/lioyan/tomcat/core/StandardHost.java
+++ spring-boot-web/src/main/java/cn/lioyan/tomcat/core/StandardHost.java
@@ -1,8 +1,18 @@
 package cn.lioyan.tomcat.core;
 
+import org.apache.catalina.valves.ErrorReportValve;
+
+
 /**
  * {@link org.apache.catalina.core.StandardHost}
+ * <pre>
+ * 内部包含两个Value {@link ErrorReportValve} 为对异常清理的处理。
+ * {@link  org.apache.catalina.core.StandardHostValve}   StandardHostValve 为调用 Context。
  *
+ *
+ *
+ *
+ * </pre>
  * @author com.lioyan
  * @date 2023/2/8  17:12
  */
